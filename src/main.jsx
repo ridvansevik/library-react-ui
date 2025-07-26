26993524064a3c36b6dcb4c331ff548a9f9df200
Ridvan Sevik <ridvan.sevik.tr@gmail.com>
2025-07-26 15:59:38 +0300

    chore(app): wrap App component with AuthProvider context

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bu satırı ekleyin
 import './index.css'; // Varsa kendi temel stilleriniz
+import { AuthProvider } from './contexts/AuthContexts.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </React.StrictMode>
 );
